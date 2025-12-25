5e08fa45e4fac4d984401d1375d1cb20bc348540
phuonglatoi <phuonglatoi@users.noreply.github.com>
2025-12-25 23:33:36 +0700

    chore: Remove cleanup script

---
+++ final_cleanup.ps1
@@ -0,0 +1,5 @@
+git add -A
+git commit -m "chore: Remove cleanup script"
+git push https://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@github.com/phuonglatoi/phongtro-attt.git main
+Write-Host "Done!" -ForegroundColor Green
+
