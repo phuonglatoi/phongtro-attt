27bb48d0831b11471342f073a9bab3a8a2bff610
phuonglatoi <phuonglatoi@users.noreply.github.com>
2025-12-25 23:38:05 +0700

    chore: Remove push_cleanup script

--- push_cleanup.ps1
+++
@@ -1,5 +0,0 @@
-git add -A
-git commit -m "chore: Remove ngrok files and unused scripts"
-git push https://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@github.com/phuonglatoi/phongtro-attt.git main
-Write-Host "Cleaned up!" -ForegroundColor Green
-
