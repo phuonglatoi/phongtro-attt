ab24d75ac4dcab1e8d5e14be2daa10d4a9062601
phuonglatoi <phuonglatoi@users.noreply.github.com>
2025-12-25 23:35:00 +0700

    chore: Final cleanup - remove all temporary scripts

--- final_cleanup.ps1
+++
@@ -1,5 +0,0 @@
-git add -A
-git commit -m "chore: Remove cleanup script"
-git push https://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@github.com/phuonglatoi/phongtro-attt.git main
-Write-Host "Done!" -ForegroundColor Green
-
