1acb73fb1ca6a88754f6dd9c445cad13d1b61038
phuonglatoi <phuonglatoi@users.noreply.github.com>
2025-12-25 23:36:35 +0700

    chore: Remove ngrok files and unused scripts

--- last_push.ps1
+++
@@ -1,6 +0,0 @@
-git add -A
-git commit -m "chore: Final cleanup - remove all temporary scripts"
-git push https://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@github.com/phuonglatoi/phongtro-attt.git main
-Remove-Item "last_push.ps1" -Force
-Write-Host "All cleaned up!" -ForegroundColor Green
-
---
+++ push_cleanup.ps1
@@ -0,0 +1,5 @@
+git add -A
+git commit -m "chore: Remove ngrok files and unused scripts"
+git push https://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@github.com/phuonglatoi/phongtro-attt.git main
+Write-Host "Cleaned up!" -ForegroundColor Green
+
